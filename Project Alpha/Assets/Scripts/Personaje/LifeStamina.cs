2a7b3d115e6c053a955e9f6463ec899f94bf4a5d
Mario García Barrero <m.garciaba.2019@alumnos.urjc.es>
2022-10-04 01:11:40 +0200

    Vida y Stamina

--- Project Alpha/Assets/Scripts/Personaje/LifeStamina.cs
+++ Project Alpha/Assets/Scripts/Personaje/LifeStamina.cs
@@ -1,29 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class LifeStamina : MonoBehaviour
 {
 
-    public int stamina;
-    public int life;
-
-    public GameObject lifeBar;
-    public GameObject staminaBar;
-
     public Slider lifeSlide;
     public Slider staminaSlide;
 
+    public float vidaMaxima;
+    public float staminaMaxima;
 
     public bool perderVida;
     public float cantidadVida;
 
+    public bool perderStamina;
+    public float cantidadStamina;
+
     // Start is called before the first frame update
     void Start()
     {
-        lifeSlide = lifeBar.GetComponent<Slider>();
-        staminaSlide = staminaBar.GetComponent<Slider>();
+        lifeSlide.value = vidaMaxima;
+        staminaSlide.value = staminaMaxima;
     }
 
     // Update is called once per frame
@@ -33,13 +32,64 @@ public class LifeStamina : MonoBehaviour
         {
             lifeLost(cantidadVida);
         }
+
+        if (perderStamina)
+        {
+            staminaLost(cantidadStamina);
+        }
     }
 
     void lifeLost(float vida)
     {
+        /*float resta = 0;
+        while (resta < vida / 2)
+        {
+            lifeSlide.value -= resta;
+            resta += 0.01f;
+        }
+
+        while (resta <= vida)
+        {
+            lifeSlide.value -= resta;
+            resta -= 0.01f;
+        }*/
+
+        /*float cantidadPerdida = 0;
+       
+        while (cantidadPerdida <= vida)
+        {
+            float i = 0;
+
+            lifeSlide.value -= i;
+
+            cantidadPerdida += i;
+
+            i++;
+        }*/
 
         lifeSlide.value -= vida;
+
         perderVida = false;
+
+    }
+
+    void staminaLost(float stamina)
+    {
+        /*float resta = 0;
+        while (resta < vida / 2)
+        {
+            lifeSlide.value -= resta;
+            resta += 0.01f;
+        }
+
+        while (resta <= vida)
+        {
+            lifeSlide.value -= resta;
+            resta -= 0.01f;
+        }*/
+        staminaSlide.value -= stamina;
+
+        perderStamina = false;
 
     }
 }

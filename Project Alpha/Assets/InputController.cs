887270c541eac1e47addb592ed1838b6478e3bca
Rubenob <rubenob@hotmail.es>
2022-11-15 01:20:47 +0100

    Mapa 2 y su decoración

--- Project Alpha/Assets/InputController.cs
+++ Project Alpha/Assets/InputController.cs
@@ -5,21 +5,14 @@ using UnityEngine;
 public class InputController : MonoBehaviour
 { 
     public Joystick joysticMove;
-
-
     public Rigidbody rb;
 
-
     float speed = 10f;
-
 
     void Move()
     {
         rb.velocity = new Vector3(joysticMove.Horizontal * speed, rb.velocity.y, joysticMove.Vertical * speed);
     }
-
-
-
 
     private void Update()
     {

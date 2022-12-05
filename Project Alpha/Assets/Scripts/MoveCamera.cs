760ca0be7a15ace354a579942bce21df4d459ac5
Mario García Barrero <m.garciaba.2019@alumnos.urjc.es>
2022-12-05 02:38:49 +0100

    Mover camara movil mejorado

--- Project Alpha/Assets/Scripts/MoveCamera.cs
+++ Project Alpha/Assets/Scripts/MoveCamera.cs
@@ -12,6 +12,8 @@ public class MoveCamera : MonoBehaviour
     public Vector2 touchDeltaPosition;
     public float speedRotationCamera;
 
+    public Joystick joystick;
+
     private void Awake()
     {
         Disp = FindObjectOfType<Dispositivo>().dispositivo;
@@ -21,13 +23,22 @@ public class MoveCamera : MonoBehaviour
     {
         if(Disp == 1)
         {
-            if (Input.touchCount == 1)
+            if (Input.touchCount == 1 && joystick.Horizontal == 0 && joystick.Vertical == 0)
             {
                 Touch touchZero = Input.GetTouch(0);
                 if (touchZero.phase == TouchPhase.Moved)
                 {
                     touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-                    cm.m_XAxis.Value += touchDeltaPosition.x * speedRotationCamera;
+                    cm.m_XAxis.Value += -touchDeltaPosition.x * speedRotationCamera;
+                }
+            }
+            else if(Input.touchCount == 2 && joystick.Horizontal != 0 || joystick.Vertical != 0)
+            {
+                Touch touchZero = Input.GetTouch(1);
+                if (touchZero.phase == TouchPhase.Moved)
+                {
+                    touchDeltaPosition = Input.GetTouch(1).deltaPosition;
+                    cm.m_XAxis.Value += -touchDeltaPosition.x * speedRotationCamera;
                 }
             }
         }
